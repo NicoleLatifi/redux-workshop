abeaa9ea73faa1b973794643e13c6c4524cfd57a
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-09-29 20:03:00 -0600

    Change back to functional component

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,16 +2,48 @@ import React, { Component } from 'react';
 import { toggleComplete } from '../actions';
 import { connect } from 'react-redux';
 
-class ToDo extends Component {
-  constructor(props) {
-    super(props)
-  }
+// class ToDo extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
+//
+//   clickToDo = (e) => {
+//     this.props.toggleComplete(this.props.id)
+//     console.log(this.props.completed)
+//   }
+//
+//   render() {
+//     const completedStatus = this.props.todo.completed ? "completed" : "not-completed"
+//     console.log(this.props)
+//     return (
+//       <li
+//         className={completedStatus}
+//         onClick={this.clickToDo}
+//       >
+//         {this.props.todo}
+//       </li>
+//     )
+//   }
+// }
 
-  render() {
-    return (
-      <li>{props.todo}</li>
-    )
-  }
+const ToDo = ({ id, todo, completed, toggleComplete }) => {
+  console.log(completed)
+  return (
+    <li
+      className={completed ? 'completed' : 'not-completed'}
+      onClick={() => toggleComplete(id)}
+    >
+    {todo}
+  </li>
+  )
 }
 
-export default ToDo;
+const mapDispatchToProps = dispatch => ({
+  toggleComplete: id => dispatch( toggleComplete(id) )
+})
+
+// const mapStateToProps = state => ({
+//   todos: state.todos
+// });
+
+export default connect(null, mapDispatchToProps)(ToDo);
